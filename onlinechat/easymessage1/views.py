5f6fa76977448826927e2de00938a0b9fba0c6f8
Erden777 <erden.aidynuly.99@gmail.com>
2021-03-11 01:12:16 +0600

    last changes

--- onlinechat/easymessage1/views.py
+++ onlinechat/easymessage1/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.template import loader
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import User
 from django.http import Http404
+
 
 def index(request):
     template = loader.get_template('easymessage/login.html')
@@ -18,5 +18,5 @@ def chat_details(request, chat_id):
     return HttpResponse("You're looking at question %s." % chat_id)
 
 def login(request):
-    print(request.POST)
+    
     return HttpResponse("hello")
